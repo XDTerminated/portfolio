5d7caeec87c7b24e070b91e215a096ed33a8de6b
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-09-21 15:30:45 -0500

    Primary Section Complete

--- src/components/ui/RoundIcon.tsx
+++ src/components/ui/RoundIcon.tsx
@@ -10,7 +10,7 @@ const RoundIcon = ({
   otherClasses?: string;
 }) => {
   return (
-    <button className="relative inline-flex h-7 w-7 animate-shimmer items-center justify-center rounded-full border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] font-medium text-slate-400 transition-colors md:mt-10">
+    <button className="relative inline-flex h-7 w-7 items-center justify-center rounded-full border-slate-800 font-medium text-slate-400 transition-colors md:mt-10">
       {position === "center" &&
         React.cloneElement(icon, {
           className: "h-full w-full rounded-full", // Ensure the icon fills the space and is circular
