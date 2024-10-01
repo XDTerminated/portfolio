0322490102bc341380b4635649ed3d6eab2e91f8
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-10-01 12:14:09 -0500

    feat: finihsed base website

---
+++ src/components/common/social-media-icons.tsx
@@ -0,0 +1,22 @@
+import { FaFacebook, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+
+import { siteConfig } from "@/config/site";
+
+export function SocialMediaIcons() {
+    return (
+        <div className="flex">
+            <a target="_blank" href={siteConfig.links.github} rel="noopener noreferrer" aria-label="Github" className="rounded px-4 py-3 text-xl hover:bg-accent hover:text-accent-foreground">
+                <FaGithub />
+            </a>
+            <a target="_blank" href={siteConfig.links.facebook} rel="noopener noreferrer" aria-label="Facebook" className="rounded px-4 py-3 text-xl hover:bg-accent hover:text-accent-foreground">
+                <FaFacebook />
+            </a>
+            <a target="_blank" href={siteConfig.links.twitter} rel="noopener noreferrer" aria-label="Twitter" className="rounded px-4 py-3 text-xl hover:bg-accent hover:text-accent-foreground">
+                <FaTwitter />
+            </a>
+            <a target="_blank" href={siteConfig.links.linkedin} rel="noopener noreferrer" aria-label="Linkedin" className="rounded px-4 py-3 text-xl hover:bg-accent hover:text-accent-foreground">
+                <FaLinkedin />
+            </a>
+        </div>
+    );
+}
---
+++ src/components/introduction.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export function Introduction() {
+    return (
+        <div className="space-y-4 p-4">
+            <p className="font-light text-muted-foreground">Welcome to /rdev!</p>
+            <p className="font-light">I'm a full-stack developer and I love open-source, I enjoy building apps in the JavaScript ecosystem and I like learning new technologies.</p>
+            <p className="font-light">
+                My favorite tech stack for building apps is <span className="font-semibold">Next.js</span>, <span className="font-semibold">React</span>, and <span className="font-semibold">TypeScript</span> for the frontend and <span className="font-semibold">Django</span> for the backend.
+            </p>
+            <p className="font-light">
+                I currently work as a <span className="font-semibold">Software Engineer</span> at <span className="font-semibold">Hiraya Technology Solutions, Inc.</span>
+            </p>
+        </div>
+    );
+}
