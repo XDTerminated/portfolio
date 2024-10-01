e016a34629a769914f69bc5d67bb4828a5d00382
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-10-01 12:25:44 -0500

    fix: updated languages and spotify card

--- src/components/dashboard/languages.tsx
+++ src/components/dashboard/languages.tsx
@@ -1,34 +1,50 @@
-import { Languages } from "@/types";
+"use client";
 
+import { Languages as LanguagesType } from "@/types";
+import { Cell, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
+
+import { languageColors } from "@/lib/language-colors";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-interface CodeTimeProps {
-    started: string;
-    totalTime: string;
-    languages: Languages[];
+interface LanguagesProps {
+    languages: LanguagesType[];
 }
 
-export function CodeTime({ started, totalTime, languages }: CodeTimeProps) {
+export function Languages({ languages }: LanguagesProps) {
+    const data = languages.slice(0, 10);
+
     return (
-        <div className="grid w-full grid-cols-1 gap-2 md:grid-cols-2">
-            <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Coding hours</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="text-lg font-bold">{totalTime}</div>
-                    <p className="text-xs text-muted-foreground">{started}</p>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Most used language</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="text-lg font-bold">{languages[0].name}</div>
-                    <p className="text-xs text-muted-foreground">{languages[0].text}</p>
-                </CardContent>
-            </Card>
-        </div>
+        <Card className="w-full">
+            <CardHeader>
+                <CardTitle className="text-lg">Top Languages</CardTitle>
+            </CardHeader>
+            <CardContent className="grid grid-cols-1 gap-2 sm:grid-cols-2 sm:gap-0">
+                <ResponsiveContainer width="100%" height={250}>
+                    <PieChart>
+                        <Pie data={data} cx="50%" cy="50%" innerRadius={50} outerRadius={100} paddingAngle={3} strokeWidth={0} dataKey="hours">
+                            {data.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={languageColors[entry.name] || "#1f9aef"} />
+                            ))}
+                        </Pie>
+                        <Tooltip formatter={(text, name, entry) => [`${entry.payload.text} (${entry.payload.percent}%)`, entry.payload.name]} />
+                    </PieChart>
+                </ResponsiveContainer>
+                <div className="flex items-center justify-center">
+                    <div className="p-4">
+                        {data.map((entry, index) => (
+                            <div key={index} className="mb-2 flex items-center text-sm">
+                                <span
+                                    className="mr-2 inline-block h-4 w-4"
+                                    style={{
+                                        backgroundColor: languageColors[entry.name] || "#1f9aef",
+                                    }}
+                                ></span>
+                                {entry.name} - {entry.text}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </CardContent>
+        </Card>
     );
 }
--- src/components/dashboard/spotify-card.tsx
+++ src/components/dashboard/spotify-card.tsx
@@ -1,34 +1,71 @@
-import { Languages } from "@/types";
+"use client";
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import { useLanyard } from "@/hooks/use-lanyard";
+import { FaSpotify } from "react-icons/fa";
 
-interface CodeTimeProps {
-    started: string;
-    totalTime: string;
-    languages: Languages[];
-}
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { buttonVariants } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { ProgressBar } from "@/components/timer/progress-bar";
 
-export function CodeTime({ started, totalTime, languages }: CodeTimeProps) {
+export function SpotifyCard() {
+    const { data, isLoading } = useLanyard();
+    const activity = data?.data?.activities.find((activity: any) => activity.name === "Spotify");
+    const trackId = data?.data?.spotify?.track_id;
+
+    if (isLoading) {
+        return (
+            <div className="w-full">
+                <div className="flex flex-col gap-2">
+                    <div className="flex gap-2">
+                        <Skeleton className="h-10 w-10 rounded-full" />
+                        <Skeleton className="h-10 w-full" />
+                    </div>
+                    <Skeleton className="h-8 w-full" />
+                </div>
+            </div>
+        );
+    }
+
+    if (!activity) {
+        return (
+            <Alert className="flex items-center justify-start gap-4 border-none">
+                <div>
+                    <FaSpotify className="h-12 w-12 text-[#1ED760]" />
+                </div>
+                <div>
+                    <AlertTitle>No activity</AlertTitle>
+                    <AlertDescription className="text-muted-foreground">Not listening to anything currently</AlertDescription>
+                </div>
+            </Alert>
+        );
+    }
+
     return (
-        <div className="grid w-full grid-cols-1 gap-2 md:grid-cols-2">
-            <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Coding hours</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="text-lg font-bold">{totalTime}</div>
-                    <p className="text-xs text-muted-foreground">{started}</p>
-                </CardContent>
-            </Card>
-            <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Most used language</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="text-lg font-bold">{languages[0].name}</div>
-                    <p className="text-xs text-muted-foreground">{languages[0].text}</p>
-                </CardContent>
-            </Card>
-        </div>
+        <Alert className="flex flex-col items-center gap-3 border-none text-center sm:flex-row sm:p-0 sm:text-left">
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger>
+                        <img src={data?.data.spotify.album_art_url} width={120} height={120} alt="Activity image" className="rounded" />
+                    </TooltipTrigger>
+                    <TooltipContent>{activity.assets ? activity.assets.large_text || activity.name : activity.name}</TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+            <div className="flex w-full flex-col items-center justify-between gap-2 sm:flex-row">
+                <div className="flex flex-col">
+                    <Link href={`https://open.spotify.com/track/${trackId}`} target="_blank">
+                        <AlertTitle className="line-clamp-1 hover:underline">{activity.details || null}</AlertTitle>
+                    </Link>
+                    <AlertDescription className="line-clamp-1">{activity.state || null}</AlertDescription>
+                    <AlertDescription className="flex justify-center sm:block">{activity.timestamps && activity.timestamps.start && activity.timestamps.end ? <ProgressBar start={activity.timestamps.start} end={activity.timestamps.end} /> : null}</AlertDescription>
+                </div>
+                <Link href={`spotify:track:${trackId}`} target="_blank" className={buttonVariants({ size: "sm" })}>
+                    <FaSpotify className="mr-2 h-4 w-4" />
+                    Play on Spotify
+                </Link>
+            </div>
+        </Alert>
     );
 }
