f953c2030922901685114b79be0fcf5beb6b2c13
Sayam Gupta <gupta.sayam2006@gmail.com>
2024-10-01 20:27:58 -0500

    Updated bio

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,7 +9,7 @@ export const siteConfig: SiteConfig = {
     url: env.NEXT_PUBLIC_APP_URL,
     ogImage: `${env.NEXT_PUBLIC_APP_URL}/og.jpg`,
     links: {
-        github: "https://github.com/redpangilinan",
+        github: "https://github.com/XDTerminated",
         facebook: "https://facebook.com/redpangilinan15",
         twitter: "https://twitter.com/_rdev7",
         linkedin: "https://www.linkedin.com/in/jan-reynald-pangilinan",
